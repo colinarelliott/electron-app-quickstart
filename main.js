8a5cd8c437809199af0cf3a479f1910a9b8e7386
Colin Elliott <colinarelliott@gmail.com>
2023-07-13 12:10:33 -0400

    added a bunch of options

--- main.js
+++ main.js
@@ -3,10 +3,24 @@ const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
+    title: 'Electron App',
+    //icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
     width: 800,
     height: 600,
+    minWidth: 400,
+    minHeight: 300,
+    maxWidth: 1200,
+    maxHeight: 900,
+    minimizable: true,
+    maximizable: true,
+    closable: true,
+    resizable: true,
+    alwaysOnTop: false,
+    frame: true,
+    disableAutoHideCursor: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      zoomFactor: 1.0,
     }
   })
 
