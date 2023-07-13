6f3525fa43ff2bcc9de42f349a029d47d3a96c6a
Colin Elliott <colinarelliott@gmail.com>
2023-07-13 11:11:48 -0400

    added variables exposed to context in preload

--- preload.js
+++ preload.js
@@ -9,3 +9,12 @@ window.addEventListener('DOMContentLoaded', () => {
       replaceText(`${dependency}-version`, process.versions[dependency])
     }
   })
+
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron
+    // we can also expose variables, not just functions
+})
