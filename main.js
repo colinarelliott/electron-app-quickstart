754eddc6267598fb9985e918d99ff548a9cca7a4
Colin Elliott <colinarelliott@gmail.com>
2023-07-13 11:53:27 -0400

    same energy

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1280,
     height: 720,
-    frame: true, // set to false to remove the frame
+    frame: false, // set to false to remove the frame
     // Basically add in related back-end scripts here
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  win.loadFile('index.html')
+  //win.loadFile('index.html')
+  win.loadURL('https://www.same.energy/')
 }
 
 app.whenReady().then(() => {
